0f3abacaed1d64cae95ac1b36bc48a9ac263c6d3
SuperFunkyNinja <superfunkyninja@gmail.com>
2021-12-31 10:42:21 +0000

    test upload

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 
 
 app = Flask(__name__)
-app.config.from_object("config.DevelopmentConfig")
+# app.config.from_object("config.DevelopmentConfig")
 app.config.from_object(os.environ["APP_SETTINGS"])
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 # from models import Result
