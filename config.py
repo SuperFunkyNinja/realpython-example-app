7e2e0ff020e227d5b40611a120f20113ac31840c
SuperFunkyNinja <superfunkyninja@gmail.com>
2021-12-31 13:44:07 +0000

    wordcount website working locally

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "this-really-needs-to-be-changed"
+    
     # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:55@localhost:5432/wordcount_dev"
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 
 
 class ProductionConfig(Config):
--- scratch.py
+++ scratch.py
@@ -1,4 +1,1 @@
-import os
-
-for k, v in os.environ.items():
-    print(f"{k}={v}")
+import nltk
